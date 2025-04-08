71b82946aa9aa864235502217f462b3935ee9728
Gianni Gabriel <clondireanug@gmail.com>
2025-04-08 16:41:49 +0200

    feat:implement message view and error page

--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -1,4 +1,6 @@
 import Router  from "express";
+import { getMessageById } from "../controller/getMessageById.js";
+
 
 const messages = [
     {
@@ -16,6 +18,7 @@ const links = [
     { href: "/" ,text:"Messages"}, 
     { href: "/new", text:"Add New Message" },  
 ];
+console.log(messages.length)
 
 export const indexRouter = Router();
 
@@ -23,10 +26,13 @@ indexRouter.get("/", (req, res) => {
     res.render("index", {links:links, messages:messages}) //renderiza index.ejs, le pasa links y los mensajes 
 })
 
+
 indexRouter.post("/new", (req, res) => {
     // desestructura la req post el body de la peticion POST, los valores tienen que coincidir con el name de los input
     const { messageText, messageUser } = req.body; 
     messages.push({ text: messageText, user: messageUser, added: new Date() });
     res.redirect("/"); //redirecciona a la ruta principal
 })
+
+indexRouter.get("/:id", getMessageById(links, messages)) //obtiene el mensaje por id, el id es el index del array messages
 
